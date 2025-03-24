f62fa5549c2f41aeeabdd854bf23db0f15b75e86
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-03-24 23:32:53 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,3 @@
-//import { useState } from "react";//
-
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
