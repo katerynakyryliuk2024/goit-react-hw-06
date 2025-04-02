63bb2231177ea0b1837b088149808e78a6d0f76a
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-04-02 17:23:52 +0300

    update ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,10 +3,15 @@ import Contact from "../Contact/Contact";
 import { useSelector } from "react-redux";
 
 export default function ContactList() {
-  const contacts = useSelector((state) => state.contacts.datas);
+  const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.filters.name);
+
+  const filteredData = contacts.filter((el) =>
+    el.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+  );
   return (
     <ul className={css.list}>
-      {contacts.map((el) => (
+      {filteredData.map((el) => (
         <li className={css.contact} key={el.id}>
           <Contact contact={el} />
         </li>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,19 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const slice = createSlice({
   name: "contacts",
   initialState: {
-    datas: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    items: [],
   },
   reducers: {
     addContact: (state, action) => {
-      state.datas.push(action.payload);
+      state.items.push(action.payload);
     },
     deleteContact: (state, action) => {
-      state.datas = state.datas.filter((data) => data.id !== action.payload);
+      state.items = state.items.filter((item) => item.id !== action.payload);
     },
   },
 });
